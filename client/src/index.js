e387ac41300f8d5365dda180a349ca291c37647a
Sam Kaessner <openglshaders@gmail.com>
2018-05-08 12:47:41 -0600

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+
+var selection = d3.select("#chart1").selectAll(".bar");
+selection.style("height", "50px");
+
+
+var p = d3.select("body")
+          .selectAll("p")
+          .data([4, 32, 23, 43, 20, 7, 86])
+          .enter()
+          .append("p")
+          .text(function(d) { return "I'm #" + d + "!"});
+
+function do_update() {
+
+}
